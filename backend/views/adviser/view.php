9a92db74bd128eaecfe4c9bba0cb39cffff7234e
ihboy <ly5569dhb@sina.cn>
2018-08-28 13:23:02 +0800

    t

--- backend/views/adviser/view.php
+++ backend/views/adviser/view.php
@@ -77,6 +77,7 @@ $this->params['breadcrumbs'][] = $this->title;
                 'position',
                 'describe',
                 'expertise',
+                'profile',
             ],
             'options' => ['class' => 'table table-hover table-noborder']
         ]) ?>
