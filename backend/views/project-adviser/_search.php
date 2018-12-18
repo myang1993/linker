d4ed13ac178dc7a32ea3a869d808ce6ba5ac72a8
ystian <1165384773@qq.com>
2018-12-18 20:13:03 +0800

    用户管理

--- backend/views/project-adviser/_search.php
+++ backend/views/project-adviser/_search.php
@@ -2,37 +2,55 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use dosamigos\datepicker\DatePicker;
 
 /* @var $this yii\web\View */
-/* @var $model backend\models\ProjectAdviserSearch */
+/* @var $model backend\models\ProjectSearch */
 /* @var $form yii\widgets\ActiveForm */
 ?>
 
-<div class="project-adviser-search">
+<div class="project-search search">
 
     <?php $form = ActiveForm::begin([
         'action' => ['index'],
         'method' => 'get',
         'options' => [
-            'data-pjax' => 1
+            'data-pjax' => 1,
+            'class' => 'form-inline form',
         ],
     ]); ?>
 
-    <?= $form->field($model, 'id') ?>
+    <?= $form->field($model, 'start_date')->widget(
+        \kartik\datetime\DateTimePicker::className(),
+        [
+            'language' => 'zh-CN',
+            'id' => 'pay_date',
+            'convertFormat' => true,
+            'options' => ['placeholder' => '请输入开始日期','style'=>'width:200px'],
+            'pluginOptions' => [
+                'autoclose' => true,
+                'format' => 'yyyy-MM-dd H:i:s',
+                'todayHighlight' => true,
+            ]
+        ]
+    )->label(Yii::t('app', 'Start Time')); ?>
 
-    <?= $form->field($model, 'project_id') ?>
+    <?= $form->field($model, 'end_date')->widget(
+        \kartik\datetime\DateTimePicker::className(),
+        [
+            'language' => 'zh-CN',
+            'id' => 'pay_date',
+            'convertFormat' => true,
+            'options' => ['placeholder' => '请输入结束日期','style'=>'width:200px'],
+            'pluginOptions' => [
+                'autoclose' => true,
+                'format' => 'yyyy-MM-dd H:i:s',
+                'todayHighlight' => true,
+            ]
+        ]
+    )->label(Yii::t('app', 'End Time')); ?>
 
-    <?= $form->field($model, 'adviser_id') ?>
-
-    <?= $form->field($model, 'state') ?>
-
-    <?= $form->field($model, 'remark') ?>
-
-    <?php // echo $form->field($model, 'pay_remark') ?>
-
-    <?php // echo $form->field($model, 'date') ?>
-
-    <div class="form-group">
+    <div class="form-group form-group-btn" style="vertical-align: top;">
         <?= Html::submitButton(Yii::t('app', 'Search'), ['class' => 'btn btn-primary']) ?>
         <?= Html::resetButton(Yii::t('app', 'Reset'), ['class' => 'btn btn-default']) ?>
     </div>
