11057e12bb1262f5a5ead3ef66cf676c1709efba
ystian <1165384773@qq.com>
2018-12-27 21:32:43 +0800

    用户

--- console/controllers/CronController.php
+++ console/controllers/CronController.php
@@ -6,6 +6,8 @@ use yii\console\Controller;
 
 class CronController extends Controller
 {
+
+    //http://www.chictr.org.cn/L3Nob3dwcm9qLmFzcHg/cHJvaj0zMzg0OA==
     public function Rand_IP()
     {
 
@@ -22,14 +24,14 @@ class CronController extends Controller
 
     public function actionPa()
     {
-        $time = 1545791549;
+        $time = 1545914743;
         while (true) {
-            for ($i = 33949; $i > 0; $i--) {
+            for ($i = 32743 - 1; $i > 0; $i--) {
                 $result = \Yii::$app->db->createCommand("select * from customer_pa where page_id = {$i}")->queryAll();
                 if ($result) {
                     continue;
                 }
-                echo $i . "\n";
+                echo 'page_id:' . $i . "\n";
                 if (time() - $time > 200) {
                     $time = time();
                 }
@@ -39,12 +41,13 @@ class CronController extends Controller
                     "Accept-Language: zh-CN,zh;q=0.9",
                     "Cache-Control:no-cache",
                     "Connection:keep-alive",
-                    "Cookie: ccpassport=4c8ed03a0ca9fe1e1d95463bea950d7f; wzwsconfirm=9e7b88ee2acc163a1ec2451d2bc8d620; wzwstemplate=NQ==; wzwschallenge=-1; wzwsvtime={$time}; onlineusercount=1",
+                    "Cookie: ccpassport=1f85e2d5e83eff2d013defaa9f2afb65; wzwsconfirm=485df71b42c119fc6f0d7a954360d580; wzwstemplate=NQ==; wzwschallenge=-1; wzwsvtime={$time}; onlineusercount=1",
                     "Host:www.chictr.org.cn",
                     "Pragma:no-cache",
                     "Referer: http://www.chictr.org.cn/searchproj.aspx",
                     "Upgrade-Insecure-Requests: 1",
                     "User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.162 Safari/537.36",
+//                    "User-Agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:64.0) Gecko/20100101 Firefox/64.0",
 //                    "X-FORWARDED-FOR:" . $this->Rand_IP() . ", CLIENT-IP:" . $this->Rand_IP()
                 ];
 
@@ -59,15 +62,18 @@ class CronController extends Controller
                 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1); //加入重定向处理,加了重定向直接空白
                 $output = curl_exec($ch);
                 curl_close($ch);
-                file_put_contents('a.' . $i . '.html', $output);
-                echo strlen($output) . "\n";
+//                file_put_contents('a.' . $i . '.html', $output);
+                echo 'length:' . strlen($output) . "\n";
+//                if (strlen($output) <= 14680) {
+//                    exit;
+//                }
                 if (strlen($output) <= 20000) {
-                    sleep(rand(10, 50));
+//                    sleep(rand(5, 30));
                     continue;
                 }
                 preg_match_all('/<div class="ProjetInfo_ms">(.*?)<\/div>/is', $output, $match);
                 if (empty($match[0])) {
-                    sleep(rand(10, 50));
+//                    sleep(rand(5, 30));
                     continue;
                 }
                 preg_match_all('/<tr(?:.*?)>(.*?)<\/tr>/is', $match[1][1], $match2);
@@ -80,8 +86,8 @@ class CronController extends Controller
                 preg_match_all('/<p(?:.*?)>(.*?)<\/p>/is', $match7[1][1], $match8);
                 $application = trim(str_replace('&nbsp;', '', $match4[1][0]));
                 $study_leadey = trim(str_replace('&nbsp;', '', $match5[1][0]));
-                $telephone = trim(str_replace('&nbsp;', '', $match6[1][1]));
-                $leadey_telephone = trim(str_replace('&nbsp;', '', $match6[1][3]));
+                $telephone = trim(str_replace(['&nbsp;', '+86', ' '], '', $match6[1][1]));
+                $leadey_telephone = trim(str_replace(['&nbsp;', '+86', ' '], '', $match6[1][3]));
                 $position = trim(str_replace('&nbsp;', '', $match8[1][0]));
                 $application_email = trim(str_replace('&nbsp;', '', $match9[1][1]));
                 $leadey__email = trim(str_replace('&nbsp;', '', $match9[1][3]));
@@ -89,7 +95,7 @@ class CronController extends Controller
                 $sql = "insert into customer_pa values(null,'{$application}','{$study_leadey}','{$telephone}','{$leadey_telephone}','{$application_email}','{$leadey__email}','{$position}',{$i},'{$date}')";
 //            echo $sql;exit;
                 \Yii::$app->db->createCommand($sql)->execute();
-                sleep(rand(10, 50));
+//                sleep(rand(5, 30));
             }
         }
     }
